e44a24091510243cbee13a5a38c0c19de3a1b646
Pablo <pabloconcasanchez@gmail.com>
2022-03-19 18:34:09 +0100

    [ADD] Actividad 3

--- src/main/java/es/cipfpbatoi/ed/u6p2/App.java
+++ src/main/java/es/cipfpbatoi/ed/u6p2/App.java
@@ -4,10 +4,13 @@ package es.cipfpbatoi.ed.u6p2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    /**
+     * Imprime un hola mundo
+     * @param args parametro args
+     */
+    public static void main( String[] args ){
+
         System.out.println( "Hello World!" );
     }
 }
